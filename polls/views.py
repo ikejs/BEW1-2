cc746e95c26d765f0bac27794e7e776db6fcb654
Ike <ikescode@gmail.com>
2019-12-03 17:46:51 -0800

    tutorial 3

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import Http404
 from django.shortcuts import render
 
 from .models import Question
@@ -9,20 +9,9 @@ def index(request):
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
-
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
-
-
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
-
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
-
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
+    try:
+        question = get_object_or_404(Question, pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
     return render(request, 'polls/detail.html', {'question': question})
